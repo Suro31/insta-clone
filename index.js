fb744ea91197d81a350ef841ced9252e53aa172e
Surojit Sarkar <149144161+Suro31@users.noreply.github.com>
2024-05-24 21:16:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import FirebaseContext from './context/firebase';
-import { firebase, FieldValue } from './lib/firebase';
-import './styles/app.css';
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import Header from './header';
+import Image from './image';
+import Actions from './actions';
+import Footer from './footer';
+import Comments from './comments';
 
-ReactDOM.render(
-  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-    <App />
-  </FirebaseContext.Provider>,
-  document.getElementById('root')
-);
+export default function Post({ content }) {
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
+
+  // components
+  // -> header, image, actions (like & comment icons), footer, comments
+  return (
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
+      <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
+      <Footer caption={content.caption} username={content.username} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}
+      />
+    </div>
+  );
+}
+
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired
+  })
+};
